f8aae875978cdbfb03a9763f94d0e509d207b5c0
Jonas <contact@atalantus.de>
2022-03-08 14:19:44 +0100

    Implement matrix_trans

--- 2D-Projective-Geometry-Engine/matrix_trans.cpp
+++ 2D-Projective-Geometry-Engine/matrix_trans.cpp
@@ -1,4 +1,5 @@
 #include "matrix_trans.h"
+#include "util.h"
 
 matrix_trans::matrix_trans()
 {
@@ -11,13 +12,32 @@ matrix_trans::matrix_trans()
 
 matrix_trans::matrix_trans(const double angle, const double move_x, const double move_y)
 {
-	double c = cos(angle);
-	double s = sin(angle);
+	const double r = util::deg_to_rad(angle);
+	double c = cos(r);
+	double s = sin(r);
 
 	m_ = {
 		{c, -s, move_x},
 		{s, c, move_y},
 		{0, 0, 1}
+	};
+}
+
+matrix_trans::matrix_trans(double a, double b, double c, double d, double e, double f)
+{
+	m_ = {
+		{a, b, c},
+		{d, e, f},
+		{0, 0, 1}
+	};
+}
+
+matrix_trans::matrix_trans(double a, double b, double c, double d, double e, double f, double g, double h, double i)
+{
+	m_ = {
+		{a, b, c},
+		{d, e, f},
+		{g, h, i}
 	};
 }
 
@@ -29,5 +49,25 @@ vector_proj matrix_trans::apply(vector_proj v) const
 		m_[0][0] * x + m_[0][1] * y + m_[0][2] * w,
 		m_[1][0] * x + m_[1][1] * y + m_[1][2] * w,
 		m_[2][0] * x + m_[2][1] * y + m_[2][2] * w
+	};
+}
+
+double matrix_trans::det() const
+{
+	return m_[0][0] * (m_[1][1] * m_[2][2] - m_[1][2] * m_[2][1])
+		- m_[0][1] * (m_[1][0] * m_[2][2] - m_[1][2] * m_[2][0])
+		+ m_[0][2] * (m_[1][0] * m_[2][1] - m_[1][1] * m_[2][0]);
+}
+
+matrix_trans matrix_trans::find_trans_matrix(vector_proj pre_image_points[4], vector_proj image_points[4])
+{
+	const vector_proj a = {1, 0, 0}, b = {0, 1, 0}, c = {0, 0, 1}, d = {1, 1, 1};
+
+	const matrix_trans m1 = {
+
+	};
+
+	const matrix_trans m2 = {
+
 	};
 }
--- 2D-Projective-Geometry-Engine/matrix_trans.h
+++ 2D-Projective-Geometry-Engine/matrix_trans.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <vector>
-
 #include "vector_proj.h"
 
 /**
@@ -17,7 +16,7 @@ public:
 	matrix_trans();
 
 	/**
-	 * \brief Initializes a transformation matrix with a given transformation.
+	 * \brief Initializes a simple transformation matrix with given rotation and translation.
 	 * \param angle Angle of the transformation
 	 * \param move_x Translation on the x-axis
 	 * \param move_y Translation of the y-axis
@@ -25,9 +24,31 @@ public:
 	matrix_trans(double angle, double move_x, double move_y);
 
 	/**
+	 * \brief Initializes an affine transformation matrix with given transformation values.
+	 */
+	matrix_trans(double a, double b, double c, double d, double e, double f);
+
+	/**
+	 * \brief Initializes a projective transformation matrix with given transformation values.
+	 */
+	matrix_trans(double a, double b, double c, double d, double e, double f, double g, double h, double i);
+
+	/**
 	 * \brief Applies the transformation a projection vector.
 	 * \param v Projection vector
 	 * \return Transformed projection vector
 	 */
-	vector_proj apply(const vector_proj v) const;
+	vector_proj apply(vector_proj v) const;
+
+	/**
+	 * \brief Calculates the determinant of this matrix.
+	 */
+	double det() const;
+
+	/**
+	 * \brief Calculates the transformation matrix used to transform 4 sample points.
+	 * \param pre_image_points The chosen 4 points previous to the transformation
+	 * \param image_points The chosen 4 points after the transformation
+	 */
+	static matrix_trans find_trans_matrix(vector_proj pre_image_points[4], vector_proj image_points[4]);
 };
